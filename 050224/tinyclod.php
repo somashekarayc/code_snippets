e78a4f5e12f3ae761a56aa61fbf97bc1f34c2cb4
som <somashekarayc@gmail.com>
2024-02-05 16:11:47 +0530

    3

--- 050224/tinyclod.php
+++ 050224/tinyclod.php
@@ -2,3 +2,7 @@ composer create-project laravel/laravel my-example-app
 
 npm install
 composer require tinymce/tinymce
+
+Add a Laravel Mix task to copy TinyMCE to the public files when Mix is run, such as:
+File: webpack.mix.js
+mix.copyDirectory('vendor/tinymce/tinymce', 'public/js/tinymce');
