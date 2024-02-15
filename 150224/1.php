208566584f2625ab7af2380d1b14b66982d2023f
som <somashekarayc@gmail.com>
2024-02-15 14:40:52 +0530

    2

--- 150224/1.php
+++ 150224/1.php
@@ -6,6 +6,65 @@
     <title>Document</title>
 </head>
 <body>
-    
-</body>
+<style>
+        .gallery-container {
+            display: flex;
+            justify-content: center;
+        }
+
+        .thumbnails {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+        }
+
+        .thumbnails img {
+            width: 28px;
+            height: 28px;
+            cursor: pointer;
+        }
+
+        .scrollbar {
+            width: 1px;
+            height: 340px;
+            background: #ccc;
+            display: block;
+            margin: 0 8px 0 0;
+        }
+
+        .thumb {
+            width: 1px;
+            position: absolute;
+            height: 340px;
+            background: #000;
+        }
+
+        .slides {
+            margin: 0 16px;
+            display: grid;
+            grid-auto-flow: row;
+            gap: 1rem;
+            width: calc(340px + 1rem);
+            padding: 0 0.25rem;
+            height: 340px;
+            overflow-y: auto;
+            overscroll-behavior-y: contain;
+            scroll-snap-type: y mandatory;
+            scrollbar-width: none;
+        }
+
+        .slides>div {
+            scroll-snap-align: start;
+        }
+
+        .slides img {
+            width: 540px;
+            object-fit: contain;
+        }
+
+        .slides::-webkit-scrollbar {
+            display: none;
+        }
+    </style>
+</head>
 </html>
