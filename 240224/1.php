25f7f906f9db5e5c0ad516adda9b240e5e4ce355
som <somashekarayc@gmail.com>
2024-02-24 12:33:04 +0530

    5

---
+++ 240224/1.php
@@ -0,0 +1,31 @@
+<!-- <br />
+<b>Warning</b>:  Undefined variable $soni in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>33</b><br />
+<br />
+<b>Warning</b>:  Undefined array key "" in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>33</b><br />
+<br />
+<b>Warning</b>:  Undefined array key 133 in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>11</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>14</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>14</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>14</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>15</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>15</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>15</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>16</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>16</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>16</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>17</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>17</b><br />
+<br />
+<b>Warning</b>:  Trying to access array offset on value of type null in <b>D:\xampp\htdocs\code_snippets\240224\1.php</b> on line <b>17</b><br />
+{"name":null,"audio":null,"description":null,"thumbnailUrl":null} -->
