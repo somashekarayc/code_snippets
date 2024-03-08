8e5d0a1488e30cdded13965d0719af4728e2d2fc
som <somashekarayc@gmail.com>
2024-03-08 14:37:40 +0530

    4

--- 080324/1.php
+++ 080324/1.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    body{text-align: center;}
+canvas {border: 1px solid black;}
+</style>
 <body>
-    
+<canvas id="drawCanvas" width="600" height="350"></canvas>
 </body>
 </html>
