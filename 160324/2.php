d3f0a74afbeffd2dc088549f14d19331b7aa5956
som <somashekarayc@gmail.com>
2024-03-16 12:27:59 +0530

    5

--- 160324/2.php
+++ 160324/2.php
@@ -1,4 +1,4 @@
-A multiline if...elseif...else statemen
+<!-- A multiline if...elseif...else statemen -->
 
 <?php
  if ($page == "Home") echo "You selected Home";
@@ -7,4 +7,25 @@ A multiline if...elseif...else statemen
  elseif ($page == "Login") echo "You selected Login";
  elseif ($page == "Links") echo "You selected Links";
  else echo "Unrecognized selection";
-?>
+
+
+// <!-- switch statemen -->
+
+switch ($page)
+ {
+ case "Home":
+ echo "You selected Home";
+ break;
+ case "About":
+ echo "You selected About";
+ break;
+ case "News":
+ echo "You selected News";
+ break;
+ case "Login":
+ echo "You selected Login";
+ break;
+ case "Links":
+ echo "You selected Links";
+ break;
+ }
