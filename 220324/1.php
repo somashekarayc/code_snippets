8ff3639f57d2dbe0a3626d74cd2858a23804e39a
som <somashekarayc@gmail.com>
2024-03-22 14:15:05 +0530

    1

---
+++ 220324/1.php
@@ -0,0 +1,10 @@
+<?php
+$p1 = array("eradu", "mooru", "naalku", "eidu");
+echo "p1 element: " . $p1[2] . "<br>";
+$p2 = array(
+    'eradu' => "eradu & Multipurpose",
+    'mooru' => "Inkjet Printer",
+    'laser' => "Laser Printer",
+    'photo' => "Photographic Paper"
+);
+echo "p2 element: " . $p2['inkjet'] . "<br>";
