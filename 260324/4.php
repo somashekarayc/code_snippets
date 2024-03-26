06bcad5a2694f7ebfb228c8ca40a7967fa9605bd
som <somashekarayc@gmail.com>
2024-03-26 17:45:52 +0530

    7

---
+++ 260324/4.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <div>
+        <div class="p-20 2xl:px-10 md:pt-6 md:px-6 md:pb-10"><button class="hidden absolute top-6 right-6 w-10 h-10 border-2 border-n-4/25 rounded-full text-0 transition-colors hover:border-transparent hover:bg-n-4/25 md:block"><svg class="inline-block w-6 h-6 fill-n-4" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M6.613 5.21l.094.083L12 10.585l5.293-5.292a1 1 0 0 1 1.497 1.32l-.083.094L13.414 12l5.293 5.293a1 1 0 0 1-1.32 1.497l-.094-.083L12 13.414l-5.293 5.293a1 1 0 0 1-1.497-1.32l.083-.094L10.585 12 5.293 6.707A1 1 0 0 1 6.511 5.14l.101.069z"></path>
+                </svg></button>
+            <div class="max-w-[58.5rem] mx-auto">
+                <div class="mb-4 h2 md:pr-16 md:h3">FAQ</div><button type="button" aria-hidden="true" style="position: fixed; top: 1px; left: 1px; width: 1px; height: 0px; padding: 0px; margin: -1px; overflow: hidden; clip: rect(0px, 0px, 0px, 0px); white-space: nowrap; border-width: 0px;"></button>
+                <div>
+                    <div id="headlessui-tabs-panel-:r2f:" role="tabpanel" tabindex="0" data-headlessui-state="selected">
+                        <div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2h:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2i:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2j:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2k:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2l:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2m:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2n:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2o:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2p:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2q:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2r:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2s:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2t:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2u:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                            <div class="border-t border-n-3 dark:border-n-6"><button class="flex w-full py-6 h6 transition-colors hover:text-primary-1 tap-highlight-color lg:hover:text-n-7 dark:lg:hover:text-n-1" id="headlessui-disclosure-button-:r2v:" type="button" aria-expanded="false" data-headlessui-state="">
+                                    <div class="relative shrink-0 w-8 h-8 mr-8 before:absolute before:top-1/2 before:l-1/2 before:w-4 before:h-0.5 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-n-6 before:rounded-full after:absolute after:top-1/2 after:l-1/2 after:w-0.5 after:h-4 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-n-6 after:rounded-full after:transition-transform after:ui-open:rotate-90 md:mr-6 dark:before:bg-n-3 dark:after:bg-n-3"></div>
+                                    <div class="text-left">one two three four five six</div>
+                                </button></div>
+                        </div>
+               
+                    </div><span id="headlessui-tabs-panel-:r2g:" role="tabpanel" tabindex="-1" style="position: fixed; top: 1px; left: 1px; width: 1px; height: 0px; padding: 0px; margin: -1px; overflow: hidden; clip: rect(0px, 0px, 0px, 0px); white-space: nowrap; border-width: 0px;"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
