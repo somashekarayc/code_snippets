8d8c0c5a6f9202e1a9429dd910e098dac2a07075
som <somashekarayc@gmail.com>
2024-03-26 11:52:57 +0530

    6

--- 260324/1.php
+++ 260324/1.php
@@ -4,7 +4,7 @@ include the script in the footer add
 
   <!--  by som -->
   <?php include __DIR__ . '/../recaptcha_script.php' ?> 
-  ==============================================
+==============================================
 
   include the validation file in the form data process
 
