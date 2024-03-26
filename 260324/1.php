3b1c457848b3b64e2b013a1a54e6606ae1453452
som <somashekarayc@gmail.com>
2024-03-26 11:44:48 +0530

    5

--- 260324/1.php
+++ 260324/1.php
@@ -1,6 +1,11 @@
 add this class to form element class="recaptcha-v-3"
-
+==============================================
 include the script in the footer add
 
   <!--  by som -->
   <?php include __DIR__ . '/../recaptcha_script.php' ?> 
+  ==============================================
+
+  include the validation file in the form data process
+
+<?php include __DIR__ . '/../recaptcha_validate.php' ?>
