7e9669d03ff3f4b1179a25c1fb69e35cf4a09b45
som <somashekarayc@gmail.com>
2024-03-27 11:36:14 +0530

    3

---
+++ 270324/2.php
@@ -0,0 +1,16 @@
+<?php 
+
+public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 2048);
+            $table->string('slug', 2048);
+            $table->string('thumbnail', 2048)->nullable();
+            $table->longText('body');
+            $table->boolean('active')->default(false);
+            $table->datetime('published_at')->nullable();
+            $table->foreignIdFor(\App\Models\User::class, 'user_id');
+            $table->timestamps();
+        });
+    }
